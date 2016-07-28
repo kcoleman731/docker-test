89ec1fabcd98f5aa65b498c5e921adb22fabd557
Kevin Coleman <kcoleman731@gmail.com>
2016-07-28 12:10:46 -0700

    Test

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		responseData, err := json.Marshal(map[string]string{"response": "pong"})
+		if err != nil {
+			fmt.Printf("Encountered Error: %v\n", err)
+		}
+		w.Write(responseData)
+	})
+	fmt.Printf("Listening on port 3000\n")
+	http.ListenAndServe(":3000", nil)
+}
